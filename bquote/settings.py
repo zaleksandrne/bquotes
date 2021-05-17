e0063c0992391b9ff7f45514afaf5c70a2b37978
Aleksandr Zagoskin <zaleksandrne@gmail.com>
2021-05-18 00:11:58 +0300

    staticsirs

--- bquote/settings.py
+++ bquote/settings.py
@@ -134,3 +134,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'app-ui/build')]
